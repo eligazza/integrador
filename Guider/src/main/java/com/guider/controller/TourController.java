b8a74bbda64d6ddc8dcd6c4145aa7ba70704a6d5
eligazza <gazzaelias@hotmail.com>
2023-10-26 14:18:01 -0300

    Added GlobalExceptionHandler and Custom Exceptions

--- Guider/src/main/java/com/guider/controller/TourController.java
+++ Guider/src/main/java/com/guider/controller/TourController.java
@@ -1,7 +1,7 @@
 package com.guider.controller;
 import com.guider.dtos.req.TourDtoReq;
 import com.guider.dtos.res.TourDtoRes;
-import com.guider.persistence.entity.Tour;
+import com.guider.exceptions.*;
 import com.guider.service.TourService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,18 +18,19 @@ public class TourController {
     private TourService tourService;
 
     @GetMapping()
-    public ResponseEntity<List<TourDtoRes>> listAll() {
+    public ResponseEntity<List<TourDtoRes>> listAll() throws NoContentException {
         List<TourDtoRes> list = tourService.listAll();
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<TourDtoRes> findTour(@PathVariable Long id) {
+    public ResponseEntity<TourDtoRes> findTour(@PathVariable Long id) throws InvalidArgumentException, MissingArgumentException, NotFoundException {
         TourDtoRes tourFounded = tourService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(tourFounded);
     }
 
     @PostMapping()
-    public ResponseEntity<TourDtoRes> saveTour(@RequestBody TourDtoReq tour) {
+    public ResponseEntity<TourDtoRes> saveTour(@RequestBody TourDtoReq tour) throws DuplicatedException, InvalidArgumentException, MissingArgumentException {
         TourDtoRes newTour = tourService.save(tour);
         return ResponseEntity.status(HttpStatus.CREATED).body(newTour);
     }
@@ -37,7 +38,7 @@ public class TourController {
     //TODO PutMapping
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<TourDtoRes> deleteTour(@PathVariable Long id) {
+    public ResponseEntity<TourDtoRes> deleteTour(@PathVariable Long id) throws NotFoundException {
         TourDtoRes deletedTour = tourService.delete(id);
         return ResponseEntity.status(HttpStatus.OK).body(deletedTour);
     }
