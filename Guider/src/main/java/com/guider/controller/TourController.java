99d3e0c4433e82fbdfa9bfd7af0bb621635b3e36
eligazza <gazzaelias@hotmail.com>
2023-10-30 23:38:27 -0300

    CORS policy accepting localhost:8080

--- Guider/src/main/java/com/guider/controller/TourController.java
+++ Guider/src/main/java/com/guider/controller/TourController.java
@@ -17,18 +17,21 @@ public class TourController {
     @Autowired
     private TourService tourService;
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping()
     public ResponseEntity<List<TourDtoRes>> listAll() throws NoContentException {
         List<TourDtoRes> list = tourService.listAll();
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/{id}")
     public ResponseEntity<TourDtoRes> findTour(@PathVariable Long id) throws InvalidArgumentException, MissingArgumentException, NotFoundException {
         TourDtoRes tourFounded = tourService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(tourFounded);
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping()
     public ResponseEntity<TourDtoRes> saveTour(@RequestBody TourDtoReq tour) throws DuplicatedException, InvalidArgumentException, MissingArgumentException {
         TourDtoRes newTour = tourService.save(tour);
@@ -37,6 +40,7 @@ public class TourController {
 
     //TODO PutMapping
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @DeleteMapping("/{id}")
     public ResponseEntity<TourDtoRes> deleteTour(@PathVariable Long id) throws NotFoundException {
         TourDtoRes deletedTour = tourService.delete(id);
